02cc44f4f02a41ecd8b49ad3a28e027e9b80d66d
s6jrlc <neo.54440+progproj@gmail.com>
2020-04-19 06:29:18 +0900

    Prepare the installation for Windows (2)

--- install.ps1
+++ install.ps1
@@ -1,1 +1,14 @@
-echo "Touch the script?"
+#echo "Touch the script?"
+
+$uri_string = "https://github.com/s6jrlc/dotfiles/archive/master.zip"
+$uri = New-Object System.Uri($uri_string)
+$file = Split-Path $uri.AbsolutePath -Leaf
+$file_path = Join-Path $env:USERPROFILE $file
+
+$cli = New-Object System.Net.WebClient
+#$cli.DownloadFile($uri, $file_path)
+#Expand-Archive -Path $file_path -DestinationPath 
+
+echo $uri_string
+echo $file
+echo $file_path
