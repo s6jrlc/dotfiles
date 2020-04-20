d9f515f62f2395af4b40c7be0ed198b907998ecc
s6jrlc <neo.54440+progproj@gmail.com>
2020-04-20 19:59:45 +0900

    prepare the installation for Windows (15)

--- install.ps1
+++ install.ps1
@@ -10,5 +10,8 @@ $vim_prof_path = Join-Path $prof_path ".vim"
 $cli = New-Object System.Net.WebClient
 $cli.DownloadFile($repo_uri, $zip_path)
 Expand-Archive -Path $zip_path -DestinationPath $env:USERPROFILE -Force
+If (Test-Path $prof_path) {
+	Remove-Item $prof_path -Recurse -Force
+}
 Rename-Item (Join-Path $env:USERPROFILE "dotfiles-master") -newName $prof_path -Force
 New-Item -Value $vim_prof_path -Path $env:USERPROFILE -Name "vimfiles" -ItemType SymbolicLink -Force
