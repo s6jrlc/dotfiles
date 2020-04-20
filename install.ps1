3f818536fe53e85570b24f679f36d194b0901abf
s6jrlc <neo.54440+progproj@gmail.com>
2020-04-20 19:11:44 +0900

    prepare the istallation for Windows (7)

--- install.ps1
+++ install.ps1
@@ -4,8 +4,8 @@ $prof_repo = "https://github.com/s6jrlc/dotfiles/archive/master.zip"
 $repo_uri = New-Object System.Uri($prof_repo)
 $prof_zip = Split-Path $repo_uri.AbsolutePath -Leaf
 $zip_path = Join-Path $env:USERPROFILE $prof_zip
-$prof_path = Join-Path $env:USERPROFILE "_dotfiles"
-$vim_prof_path = Join-Path $prof_path ".vim"
+#$prof__path = Join-Path $env:USERPROFILE "_dotfiles"
+#$vim_prof_path = Join-Path $prof_path ".vim"
 
 echo $prof_repo
 echo $prof_file
@@ -13,5 +13,5 @@ echo $prof_path
 
 $cli = New-Object System.Net.WebClient
 $cli.DownloadFile($repo_uri, $zip_path)
-Expand-Archive -Path $zip_path -DestinationPath $prof_path -Force
-New-Item -Value $vim_prof_path -Path $env:USERPROFILE -Name "_vimfiles" -ItemType SymbolicLink
+Expand-Archive -Path $zip_path -DestinationPath $env:USERPROFILE -Force
+#New-Item -Value $vim_prof_path -Path $env:USERPROFILE -Name "_vimfiles" -ItemType SymbolicLink
