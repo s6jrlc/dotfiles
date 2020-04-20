0413350e0e4fdb4a390a027a243bfcf1af969cfc
s6jrlc <neo.54440+progproj@gmail.com>
2020-04-20 18:55:49 +0900

    prepare the installation for Windows (5)

--- install.ps1
+++ install.ps1
@@ -3,7 +3,7 @@
 $prof_repo = "https://github.com/s6jrlc/dotfiles/archive/master.zip"
 $repo_uri = New-Object System.Uri($prof_repo)
 $prof_file = Split-Path $repo_uri.AbsolutePath -Leaf
-$prof_path = Join-Path $env:USERPROFILE $file
+$prof_path = Join-Path $env:USERPROFILE $prof_file
 
 echo $prof_repo
 echo $prof_file
